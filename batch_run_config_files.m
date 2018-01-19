2caa62971cb662f1bceb40c4eca2a90d05501756
orrblue <nitzan.orr@gmail.com>
2018-01-18 23:50:38 -0800

    fix names, comments, documentation

--- batch_run_config_files.m
+++ batch_run_config_files.m
@@ -1,7 +1,8 @@
 %{
  batch_run_config_files.m
 
- Runs all configuration files in the given directory and returns the specified result outputs 
+ Runs all configuration files in the given directory and returns the
+ specified results in a directory inside given directory.
  
  INPUTS:
  directory - absolute path of directory containing configuration files to be run
@@ -13,6 +14,7 @@
 
  OUTPUT:
  struct with info about experiment and includes 3D matrix of results
+ Matrix is num_subnetworks * num_samples * num_runs
 
 %}
 
@@ -21,9 +23,8 @@
 
 %{ 
  TODO:
-0. Test directory input, might not be working yet
 1. Support other types of outputs and find elegant way to implement feature
-2. Test function
+2. Test function?
 3. Find best way to represent output data
 4. Make sure it's okay to use ScenarioContainer instead of Scenario
 %}
@@ -44,35 +45,31 @@ end
 
 
 
-function output_matrix = get_results(file_name, directory, run_times, sampling_interval, duration, output_type)
+function [] = get_results(file_name, directory, run_times, sampling_interval, duration, output_type)
     
-    
-    % here = fileparts(mfilename('fullpath'));
     config_file = fullfile(directory, file_name);
-
     xml = ScenarioContainer(config_file);
     num_subnetworks = numel(xml.scenario.scenario.subnetworks.subnetwork);
-    subs_ids = arrayfun(@(z) z.ATTRIBUTE.id, xml.scenario.scenario.subnetworks.subnetwork);
+    subnetwork_ids = arrayfun(@(z) z.ATTRIBUTE.id, xml.scenario.scenario.subnetworks.subnetwork);
     
-    % output matrix of num subnets * num samples * num runs
+    % output matrix of num subnetworks * num samples * num runs
     output_matrix = zeros(num_subnetworks, (duration/sampling_interval), run_times);
     beats = BeATSWrapper(config_file);
     
     for counter = 1:num_subnetworks
             if output_type == 'travel_time'
-                beats.api.request_path_travel_time(subs_ids(counter), sampling_interval)
+                beats.api.request_path_travel_time(subnetwork_ids(counter), sampling_interval)
             end
     end
     
     for run_counter = 1:run_times
-
-        beats.run_simple(0, duration)
+        beats.run_simple(0, duration);
         output_data = beats.api.get_output_data();
         data_iter = output_data.iterator;
 
         for counter = 1:num_subnetworks
             data = data_iter.next();
-            for time = sampling_interval : sampling_interval : duration %begin time after one output frequency has passed
+            for time = sampling_interval : sampling_interval : duration % begin time after one sampling_interval has passed
                 if output_type == 'travel_time'
                     output_matrix(counter, (time/sampling_interval), run_counter) = data.compute_travel_time_for_start_time(time);
                 end
@@ -86,7 +83,7 @@ function output_matrix = get_results(file_name, directory, run_times, sampling_i
     result.runs = run_times;
     result.sampling_interval = sampling_interval;
     result.duration = duration;
-    result.subnetwork_ids = subs_ids;
+    result.subnetwork_ids = subnetwork_ids;
     
     
     [~, ~] = mkdir(directory, 'experiment_results');
