48af2d034eae60d08b9c9148585dbf1dc89456f1
orrblue <nitzan.orr@gmail.com>
2018-01-22 21:57:46 -0800

    finished

--- gen_random_offsets.m
+++ gen_random_offsets.m
@@ -1,12 +1,15 @@
 %{
  gen_random_offsets.m
 
- Creates random offsets for a configuration file and saves new configuration file at current directory 
+ Calculates randomized offsets for a specified configuration file and saves 
+ new configuration file. New file is saved in a directory on the same level 
+ as the original file. Directory is called 'randomized_config_files' and is
+ created if not already present. 
  
  INPUTS:
- config_file - name of config_file (in current directory) to be used
- cycle_time - cycle time of signal so random offsets are crated in [0, cycle_time - 1]
- save_name - name for newly saved config file
+ config_file - Absolute path to config file to be used
+ cycle_time - the cycle time of the signal. Required so that random offsets 
+    are created in [0, cycle_time - 1]
 
  OUTPUT:
  None
@@ -18,19 +21,18 @@
 
 
 %{
- Possible bug: 
- if target_actuator in xml has more than one id, this might not work
+ Possible bug?: 
+ If target_actuator in xml can have more than one target_actuator id, or 
+ multiple schedule items (for different nodes?), then this might not work.
+ If there are multiple schedule items, do they have different start times?
 
 %}
 
-function [] = gen_random_offsets(config_file, cycle_time, save_name)
-
-    here = fileparts(mfilename('fullpath'));
-    config_file = fullfile(here, config_file);
-
+function [] = gen_random_offsets(config_file, cycle_time)
+    config_file = fullfile(config_file);    
     xml = ScenarioContainer(config_file);
-    controller_ids = arrayfun(@(z) z.ATTRIBUTE.id , xml.scenario.scenario.controllers.controller)
-    node_ids = arrayfun(@(z) z.target_actuators.ATTRIBUTE.ids , xml.scenario.scenario.controllers.controller)
+    controller_ids = arrayfun(@(z) z.ATTRIBUTE.id, xml.scenario.scenario.controllers.controller);
+    node_ids = arrayfun(@(z) z.target_actuators.ATTRIBUTE.ids, xml.scenario.scenario.controllers.controller);
 
     % generate enough offsets for each target_actuator
     rng('shuffle');
@@ -39,17 +41,21 @@ function [] = gen_random_offsets(config_file, cycle_time, save_name)
     i = 1;
     for node_id = node_ids
         offset = offsets(i);
-        i = i + 1;
+        
         
         % check node_id matches and set offset on node_id
-        cid = X.scenario.get_controllerid_for_nodeid(node_id);
+        cid = xml.scenario.get_controllerid_for_nodeid(node_id);
         cind = cid==controller_ids;
         if sum(cind) ~= 1
             error('sum(cind)~=1')
         end
-        X.scenario.scenario.controllers.controller(cind).schedule.schedule_item.ATTRIBUTE.offset = offset;
+        
+        i = i + 1;
+        xml.scenario.scenario.controllers.controller(cind).schedule.schedule_item.ATTRIBUTE.offset = offset;
     end
     
-    X.scenario.save(fullfile(here,save_name));
+    [path, name]=fileparts(config_file);
+    [~, ~] = mkdir(path, 'randomized_config_files');
+    xml.scenario.save(fullfile(path, 'randomized_config_files', ['rand_', name]));
 end
 
